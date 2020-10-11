b030adb3dc37d38728dd2566f157875ecc4dee1b
Radiant <you@example.com>
2020-10-12 06:02:01 +0700

    rombak register

--- source/components/Register.js
+++ source/components/Register.js
@@ -6,6 +6,8 @@ import {
   View,
   Image,
   Picker,
+  ViewPropTypes,
+  ScrollView,
 } from 'react-native';
 import styles from './styleRegister';
 import fb from '../assets/fb.png';
@@ -13,92 +15,103 @@ import google from '../assets/google.png';
 import flag from '../assets/indoflag.png';
 import eyeOpened from '../assets/eyeOpened.png';
 import back from '../assets/back.png';
+import styleLogin from './styleLogin';
 
 export class Register extends Component {
   render() {
     return (
       <View style={styles.mainView}>
-        <View style={styles.subView}>
-          <TouchableOpacity
-            onPress={() => this.props.navigation.navigate('Login')}>
-            <Image source={back} style={styles.backImg} />
-          </TouchableOpacity>
-          <Text style={styles.mainText}>Registration</Text>
-          <View style={styles.subView2}>
-            <Text style={styles.subText}>Already have an account?</Text>
+        <ScrollView>
+          <View style={styles.subView}>
             <TouchableOpacity
               onPress={() => this.props.navigation.navigate('Login')}>
-              <Text style={styles.subText2}>Sign In</Text>
+              <Image source={back} style={styles.backImg} />
             </TouchableOpacity>
-          </View>
-          <Text style={styles.emailText}>Email</Text>
-          <View style={styles.viewInput}>
-            <View style={styles.subViewInput}>
-              <TextInput placeholder="youremail@example.com" />
-            </View>
-          </View>
-          <Text style={styles.mobileText}>Mobile Number</Text>
-          <View style={styles.viewMobile}>
-            <View style={styles.viewInput2}>
-              <View style={styles.subViewInput5}>
-                <Image source={flag} style={styles.flag} />
-                <Text>+62</Text>
-                <Picker />
+            <Text style={styles.mainText}>Registration</Text>
+            <Text style={styles.textSignIn}>Already have an account?</Text>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate('Login')}>
+              <Text style={styles.subTextSignIn}>Sign In</Text>
+            </TouchableOpacity>
+            <Text>Email</Text>
+            <View style={styles.viewEmail}>
+              <View style={styles.subViewEmail}>
+                <TextInput placeholder="email@example.com"></TextInput>
               </View>
             </View>
-            <View style={styles.viewInput3}>
-              <View style={styles.subViewInput3}>
-                <TextInput placeholder="Phone Number"></TextInput>
-              </View>
-            </View>
-          </View>
-          <Text style={styles.passText}>Password</Text>
-          <View style={styles.viewInput}>
-            <View style={styles.subViewInput4}>
-              <View style={styles.subTextInput}>
-                <TextInput placeholder="Your_Password" secureTextEntry={true} />
-              </View>
-              {/* <Image source={eyeOpened} style={styles.flag} /> */}
-            </View>
-          </View>
-          <TouchableOpacity
-            onPress={() => this.props.navigation.navigate('Home')}>
-            <View style={styles.loginView}>
-              <Text style={styles.loginText}>Sign Up</Text>
-            </View>
-          </TouchableOpacity>
-          <Text style={styles.subText3}>Log in with social account</Text>
-          <View style={styles.viewSocial}>
-            <View>
-              <TouchableOpacity>
-                <View style={styles.fbLogin}>
-                  <Image source={fb} style={styles.fb} />
-                  <Text style={styles.loginText}>Facebook</Text>
+            <View style={styles.mainViewNumber}>
+              <Text>Mobile Number</Text>
+              <View style={styles.viewNumber}>
+                <View style={styles.subViewNumber}>
+                  <View style={styles.subSubViewNumber}>
+                    <Image source={flag} style={styles.flag} />
+                    <Text> +62</Text>
+                  </View>
                 </View>
-              </TouchableOpacity>
-            </View>
-            <View>
-              <TouchableOpacity>
-                <View style={styles.googleLogin}>
-                  <Image source={google} style={styles.google} />
-                  <Text style={styles.loginText}>Google</Text>
+                <View style={styles.subViewNumber}>
+                  <View style={styles.subSubViewNumber2}>
+                    <TextInput placeholder="081234"></TextInput>
+                  </View>
                 </View>
-              </TouchableOpacity>
+              </View>
             </View>
-          </View>
-          <View style={styles.subView3}>
-            <Text style={styles.subText}>
-              By clicking sign up you are agreeing to the
-              <TouchableOpacity>
-                <Text style={styles.subText2}>Term of use</Text>
-              </TouchableOpacity>
-              |
-              <TouchableOpacity>
-                <Text style={styles.subText2}>Privacy policy</Text>
-              </TouchableOpacity>
+            <View style={styles.mainViewPass}>
+              <Text>Password</Text>
+              <View style={styles.viewPass}>
+                <View style={styles.subViewPass}>
+                  <TextInput
+                    placeholder="your_Password"
+                    secureTextEntry={true}
+                    style={styles.textInput}></TextInput>
+                  <Image source={eyeOpened} style={styles.eye} />
+                </View>
+              </View>
+            </View>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate('Home')}>
+              <View style={styles.viewSignUp}>
+                <Text style={styles.textSignUp}>Sign Up</Text>
+              </View>
+            </TouchableOpacity>
+            <Text style={styles.textSocial}>
+              ---------------- Log in with social account ----------------
             </Text>
+            <View style={styles.mainViewSocial}>
+              <TouchableOpacity>
+                <View style={styles.viewSocial}>
+                  <View style={styles.subViewSocial}>
+                    <Image source={fb} style={styles.fb} />
+                    <Text> Facebook</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <View style={styles.viewSocial2}>
+                  <View style={styles.subViewSocial2}>
+                    <Image source={google} style={styles.google} />
+                    <Text> Google</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.viewTerms}>
+              <Text style={styles.textTerms}>
+                By clicking sign up you are agreeing to the{' '}
+                <TouchableOpacity>
+                  <Text style={{color: 'blue', fontWeight: 'bold'}}>
+                    Term of Use
+                  </Text>
+                </TouchableOpacity>
+                |
+                <TouchableOpacity>
+                  <Text style={{color: 'blue', fontWeight: 'bold'}}>
+                    Privacy Policy
+                  </Text>
+                </TouchableOpacity>
+              </Text>
+            </View>
           </View>
-        </View>
+        </ScrollView>
       </View>
     );
   }
--- source/components/styleRegister.js
+++ source/components/styleRegister.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 export default StyleSheet.create({
   mainView: {
@@ -12,142 +12,133 @@ export default StyleSheet.create({
   mainText: {
     fontSize: 30,
     fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  subView2: {
-    flexDirection: 'row',
-  },
-  subView3: {
-    marginTop: 20,
-    justifyContent: 'center',
-  },
-  subText: {
-    textAlign: 'center',
-    color: 'grey',
-  },
-  subText2: {
-    marginHorizontal: 5,
-    textAlign: 'center',
-    color: 'blue',
-    fontWeight: 'bold',
-  },
-  mobileText: {
-    marginTop: 10,
-    marginBottom: 5,
-  },
-  viewMobile: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  emailText: {
-    marginTop: 30,
-    marginBottom: 5,
-  },
-  viewInput: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    height: 50,
-    justifyContent: 'center',
-  },
-  subViewInput: {
-    marginHorizontal: 10,
-  },
-  viewMobile: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  viewInput2: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    height: 50,
-    width: '37%',
-  },
-  subViewInput5: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginHorizontal: 15,
-    justifyContent: 'space-between',
-  },
-  flag: {
-    width: 20,
-    height: 20,
-  },
-  viewInput3: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    height: 50,
-    width: '50%',
-    justifyContent: 'center',
-  },
-  subViewInput3: {
-    marginHorizontal: 10,
-  },
-  passText: {
-    marginTop: 10,
-    marginBottom: 5,
-  },
-  subViewInput4: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  subTextInput: {
-    width: '90%',
-  },
-  loginView: {
-    backgroundColor: 'blue',
-    borderRadius: 10,
-    marginTop: 20,
-    height: 50,
-    justifyContent: 'center',
-  },
-  loginText: {
-    textAlign: 'center',
-    color: 'white',
-  },
-  subText3: {
-    textAlign: 'center',
-    color: 'grey',
-    marginTop: 40,
-    fontSize: 10,
-  },
-  viewSocial: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  fb: {
-    width: 20,
-    height: 20,
-    marginRight: 10,
-  },
-  fbLogin: {
-    backgroundColor: '#000094',
-    borderRadius: 10,
-    marginTop: 20,
-    height: 50,
-    width: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  googleLogin: {
-    backgroundColor: '#eb0000',
-    borderRadius: 10,
-    marginTop: 20,
-    height: 50,
-    width: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  google: {
-    width: 20,
-    height: 20,
-    marginRight: 10,
   },
   backImg: {
     width: 50,
     height: 50,
     marginBottom: 10,
+  },
+  textSignIn: {
+    color: 'grey',
+    marginTop: 20,
+  },
+  subTextSignIn: {
+    fontWeight: 'bold',
+    color: 'blue',
+    marginBottom: 30,
+  },
+  viewEmail: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  subViewEmail: {
+    marginHorizontal: 10,
+  },
+  mainViewNumber: {
+    marginBottom: 10,
+  },
+  viewNumber: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  subViewNumber: {
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  subSubViewNumber: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    width: 120,
+  },
+  subSubViewNumber2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    width: 190,
+  },
+  flag: {
+    height: 19,
+    width: 25,
+  },
+  mainViewPass: {
+    marginBottom: 20,
+  },
+  viewPass: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  subViewPass: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    width: '90%',
+  },
+  eye: {
+    width: 30,
+    height: 20,
+  },
+  viewSignUp: {
+    backgroundColor: 'blue',
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textSignUp: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  textSocial: {
+    color: 'grey',
+    textAlign: 'center',
+    marginTop: 30,
+    marginBottom: 10,
+  },
+  mainViewSocial: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  viewSocial: {
+    backgroundColor: 'aqua',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  subViewSocial: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+  },
+  fb: {
+    width: 20,
+    height: 20,
+  },
+  viewSocial2: {
+    backgroundColor: 'red',
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  subViewSocial2: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 50,
+  },
+  google: {
+    width: 20,
+    height: 20,
+  },
+  textTerms: {
+    textAlign: 'center',
+    marginTop: 20,
   },
 });
